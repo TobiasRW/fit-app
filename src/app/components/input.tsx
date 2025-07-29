d9a781f69e087a18e3b2636167fdcc10ef7eae4a
TobiasRW <tobiasrw98@gmail.com>
2025-07-29 10:52:38 +0200

    input component updated

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -5,7 +5,13 @@ type InputProps = {
   type?: string;
   id?: string;
   name?: string;
+  min?: string;
+  max?: string;
   required?: boolean;
+  ref?: React.Ref<HTMLInputElement>;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: () => void;
 };
 
 export default function Input({
@@ -15,7 +21,13 @@ export default function Input({
   type = "text",
   id,
   name,
+  min,
+  max,
   required = false,
+  ref,
+  value,
+  onChange,
+  onFocus,
 }: InputProps) {
   const variants = {
     outlined: "border border-gray-300 rounded-md p-2 ",
@@ -31,6 +43,12 @@ export default function Input({
         id={id}
         name={name}
         required={required}
+        min={min}
+        max={max}
+        ref={ref}
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
       />
     </div>
   );
