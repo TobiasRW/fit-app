c34e395d8ff03d2f23e24805217b62090670521a
TobiasRW <tobiasrw98@gmail.com>
2025-08-04 12:08:38 +0200

    updates to edit exercise

--- src/app/components/exercise-card.tsx
+++ src/app/components/exercise-card.tsx
@@ -129,7 +129,6 @@ export default function ExerciseCard({
         {isEditing && (
           <EditExerciseModal
             setIsEditing={setIsEditing}
-            isEditing={isEditing}
             exercise={exercise}
             planSlug={planSlug}
             workoutSlug={workoutSlug}
