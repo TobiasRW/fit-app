5a6086baeddad3e84a29cfd5856bf85c5ac56922
TobiasRW <tobiasrw98@gmail.com>
2025-08-08 16:35:29 +0200

    suspemse for stats

--- src/app/(main)/stats/actions.ts
+++ src/app/(main)/stats/actions.ts
@@ -1,6 +1,5 @@
-import { CompletedWorkout } from "@/app/types";
 import { checkAuthentication } from "@/utils/helpers/helpers";
-import { startOfWeek, endOfWeek, getISOWeek, getYear, getDay } from "date-fns";
+import { getDay } from "date-fns";
 
 export async function getTotalCompletedWorkouts(): Promise<
   number | { error: string }
@@ -39,7 +38,7 @@ export async function getUserDayOfWeekCounts(): Promise<
 
   if (error) {
     console.error("Error fetching completed workouts:", error);
-    return { error: "Failed to load completed workouts" };
+    return { error: "Failed to load your day of week stats" };
   }
 
   // Count workouts by day of week
@@ -60,7 +59,126 @@ export async function getUserDayOfWeekCounts(): Promise<
     };
   });
 
-  console.log("Day counts:", results);
-
   return results;
+}
+
+export async function getUserBenchPressPR(): Promise<
+  number | { error: string } | null
+> {
+  const { supabase, user } = await checkAuthentication();
+  const benchPressId = "e09e6102-0cd4-4b11-8774-4a7251b146a4";
+
+  // Get all completed_exercise IDs for bench press
+  const { data: exercises, error: exercisesError } = await supabase
+    .from("completed_exercises")
+    .select("id")
+    .eq("user_id", user.id)
+    .eq("exercise_id", benchPressId);
+
+  if (exercisesError) {
+    return { error: "Failed to load deadlift exercises." };
+  }
+  if (!exercises || exercises.length === 0) {
+    return null; // No PR yet
+  }
+
+  const exerciseIds = exercises.map((ex: { id: string }) => ex.id);
+
+  // Get the max weight from completed_sets for those exercises
+  const { data: sets, error: setsError } = await supabase
+    .from("completed_sets")
+    .select("weight")
+    .in("completed_exercise_id", exerciseIds)
+    .order("weight", { ascending: false })
+    .limit(1);
+
+  if (setsError) {
+    return { error: "Failed to load deadlift PR." };
+  }
+  if (!sets || sets.length === 0) {
+    return null; // No PR yet
+  }
+
+  return Number(sets[0].weight);
+}
+
+// Squat PR
+export async function getUserSquatPR(): Promise<
+  number | { error: string } | null
+> {
+  const { supabase, user } = await checkAuthentication();
+  const squatId = "8ec9d613-55e8-4598-b37b-7bb45ad0ab20";
+
+  // Get all completed_exercise IDs for squat
+  const { data: exercises, error: exercisesError } = await supabase
+    .from("completed_exercises")
+    .select("id")
+    .eq("user_id", user.id)
+    .eq("exercise_id", squatId);
+
+  if (exercisesError) {
+    return { error: "Failed to load deadlift exercises." };
+  }
+  if (!exercises || exercises.length === 0) {
+    return null; // No PR yet
+  }
+
+  const exerciseIds = exercises.map((ex: { id: string }) => ex.id);
+
+  // Get the max weight from completed_sets for those exercises
+  const { data: sets, error: setsError } = await supabase
+    .from("completed_sets")
+    .select("weight")
+    .in("completed_exercise_id", exerciseIds)
+    .order("weight", { ascending: false })
+    .limit(1);
+  if (setsError) {
+    return { error: "Failed to load deadlift PR." };
+  }
+  if (!sets || sets.length === 0) {
+    return null; // No PR yet
+  }
+
+  return Number(sets[0].weight);
+}
+
+// Deadlift pr
+export async function getUserDeadliftPR(): Promise<
+  number | { error: string } | null
+> {
+  const { supabase, user } = await checkAuthentication();
+  const deadliftId = "aa9ccfd3-d333-40cc-a3df-ad6d3ce5c800";
+
+  // Get all completed_exercise IDs for deadlift
+  const { data: exercises, error: exercisesError } = await supabase
+    .from("completed_exercises")
+    .select("id")
+    .eq("user_id", user.id)
+    .eq("exercise_id", deadliftId);
+
+  if (exercisesError) {
+    return { error: "Failed to load deadlift exercises." };
+  }
+  if (!exercises || exercises.length === 0) {
+    return null; // No PR yet
+  }
+
+  const exerciseIds = exercises.map((ex: { id: string }) => ex.id);
+
+  // Get the max weight from completed_sets for those exercises
+  const { data: sets, error: setsError } = await supabase
+    .from("completed_sets")
+    .select("weight")
+    .in("completed_exercise_id", exerciseIds)
+    .order("weight", { ascending: false })
+    .limit(1);
+
+  if (setsError) {
+    return { error: "Failed to load deadlift PR." };
+  }
+  if (!sets || sets.length === 0) {
+    return null; // No PR yet
+  }
+
+  return Number(sets[0].weight);
 }
--- src/app/(main)/stats/page.tsx
+++ src/app/(main)/stats/page.tsx
@@ -1,18 +1,141 @@
+import { Suspense } from "react";
 import { getUserWeekStreak } from "../actions";
-import { getTotalCompletedWorkouts, getUserDayOfWeekCounts } from "./actions";
+import { getUserGoal } from "../profile/actions";
+import {
+  getTotalCompletedWorkouts,
+  getUserBenchPressPR,
+  getUserDayOfWeekCounts,
+  getUserDeadliftPR,
+  getUserSquatPR,
+} from "./actions";
 import ErrorCard from "@/app/components/error-card";
 
-export default async function Page() {
-  const dayCounts = await getUserDayOfWeekCounts();
+const prDefinitions = [
+  { name: "Bench Press", fetch: getUserBenchPressPR },
+  { name: "Squat", fetch: getUserSquatPR },
+  { name: "Deadlift", fetch: getUserDeadliftPR },
+];
 
-  if (!Array.isArray(dayCounts)) {
+export default async function Page() {
+  return (
+    <>
+      <main className="mx-auto mt-10 w-11/12 pb-30">
+        <h1 className="text-4xl font-bold">Your Stats</h1>
+
+        <section className="mt-10">
+          <div className="flex justify-between">
+            <Suspense fallback={<LoadingTotalSquare />}>
+              <TotalSquare />
+            </Suspense>
+            <Suspense fallback={<LoadingStreakSquare />}>
+              <StreakSquare />
+            </Suspense>
+          </div>
+          <Suspense fallback={<LoadingBarChart />}>
+            <BarChart />
+          </Suspense>
+        </section>
+        <section className="mt-10">
+          <h3 className="mb-2 text-xl font-medium"> This year</h3>
+          <hr className="border-foreground/20 relative right-1/2 left-1/2 -mr-[50vw] -ml-[50vw] w-screen border-t" />
+          <Suspense fallback={<LoadingWorkoutCompletion />}>
+            <WorkoutYearCompletion />
+          </Suspense>
+        </section>
+
+        <section className="mt-10">
+          <h3 className="mb-2 text-xl font-medium">Your PR&#39;s</h3>
+          <hr className="border-foreground/20 relative right-1/2 left-1/2 -mr-[50vw] -ml-[50vw] w-screen border-t" />
+          <div className="justify-betwee mt-4 flex items-center gap-4">
+            {prDefinitions.map((def) => (
+              <Suspense key={def.name} fallback={<LoadingPR />}>
+                <PRCard name={def.name} fetch={def.fetch} />
+              </Suspense>
+            ))}
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+async function PRCard({
+  name,
+  fetch,
+}: {
+  name: string;
+  fetch: () => Promise<number | { error: string } | null>;
+}) {
+  const pr = await fetch();
+
+  if (typeof pr === "object" && pr !== null && "error" in pr) {
     return (
-      <div className="h-40 w-full">
-        <ErrorCard errorText={dayCounts.error} variant="secondary" />
+      <div className="h-26 w-26">
+        <ErrorCard errorText={"Failed to Load PR"} variant="secondary" />
       </div>
     );
   }
 
+  return (
+    <div className="bg-gray text-foreground flex h-26 w-26 flex-col items-center justify-between rounded-lg p-2 shadow-lg">
+      <div className="justify-center text-center">
+        <p className="mb-4 text-sm">{name}</p>
+        <p className={`text-4xl ${!pr ? "text-lg" : ""}`}>
+          {pr?.toString() || "No PR Yet"}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+async function WorkoutYearCompletion() {
+  const totalWorkouts = await getTotalCompletedWorkouts();
+  const goal = await getUserGoal();
+
+  if (typeof totalWorkouts === "object" && "error" in totalWorkouts) {
+    return (
+      <div className="mt-10 h-24 w-full">
+        <ErrorCard
+          errorText={"Failed to load your total workouts completed"}
+          variant="secondary"
+        />
+      </div>
+    );
+  }
+  if ("error" in goal) {
+    return (
+      <div className="mt-10 h-24 w-full">
+        <ErrorCard errorText={"Failed to load your goal"} variant="secondary" />
+      </div>
+    );
+  }
+
+  const totalYearGoal = goal.workout_goal_per_week * 52;
+
+  return (
+    <div className="mt-4">
+      <p className="mb-2 font-light italic">
+        {totalWorkouts} / {totalYearGoal} workouts completed
+      </p>
+      <div className="bg-faded-green relative h-3 w-full rounded-sm">
+        <div
+          className="bg-green absolute inset-0 rounded-sm"
+          style={{ width: `${(totalWorkouts / totalYearGoal) * 100}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+}
+
+async function BarChart() {
+  const dayCounts = await getUserDayOfWeekCounts();
+  if (!Array.isArray(dayCounts)) {
+    return (
+      <div className="mt-10 h-40 w-full">
+        <ErrorCard errorText={dayCounts.error} variant="secondary" />
+      </div>
+    );
+  }
   // Calculate percentages
   const totalWorkouts = dayCounts.reduce((sum, d) => sum + d.count, 0);
   const dayPercentages = dayCounts.map((d) => ({
@@ -21,52 +144,39 @@ export default async function Page() {
   }));
 
   return (
-    <>
-      <main className="mx-auto mt-10 w-11/12">
-        <h1 className="text-4xl font-bold">Your Stats</h1>
-
-        <section className="mt-10">
-          <div className="flex justify-between">
-            <TotalSquare />
-            <StreakSquare />
-          </div>
-          <div className="mt-10">
-            <h3 className="mb-4 text-lg font-semibold">Most active days</h3>
-            <div className="">
-              <div className="relative mx-auto flex max-h-80 w-full items-end justify-between gap-2">
-                {dayPercentages.map((day, i) => (
-                  <div
-                    key={day.day}
-                    className="flex h-full flex-col items-center justify-end"
-                  >
-                    <div className="mb-2">
-                      <p className="text-center text-xs font-light italic">
-                        {day.percentage.toFixed(0)}%
-                      </p>
-                      <div
-                        className="bg-green w-8 rounded-t"
-                        style={{
-                          height: `${day.percentage * 1.5}px`,
-                          minHeight: "0px",
-                        }}
-                        title={`${day.percentage.toFixed(1)}%`}
-                      />
-                    </div>
-                    <div className="-rotate-45">
-                      <span className="mt-2 text-xs font-light">
-                        {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][i]}
-                      </span>
-                    </div>
-                  </div>
-                ))}
-                <div className="border-green absolute bottom-8 w-full border-b"></div>
+    <div className="mt-10">
+      <h2 className="mb-4 text-lg font-semibold">Most active days</h2>
+      <div className="">
+        <div className="relative mx-auto flex max-h-80 w-full items-end justify-between gap-2">
+          {dayPercentages.map((day, i) => (
+            <div
+              key={day.day}
+              className="flex h-full flex-col items-center justify-end"
+            >
+              <div className="mb-2">
+                <p className="text-center text-xs font-light italic">
+                  {day.percentage.toFixed(0)}%
+                </p>
+                <div
+                  className="bg-green w-8 rounded-t"
+                  style={{
+                    height: `${day.percentage * 1.5}px`,
+                    minHeight: "0px",
+                  }}
+                  title={`${day.percentage.toFixed(1)}%`}
+                />
+              </div>
+              <div className="-rotate-45">
+                <span className="mt-2 text-xs font-light">
+                  {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][i]}
+                </span>
               </div>
             </div>
-          </div>
-        </section>
-        {/* <hr className="border-foreground/20 relative right-1/2 left-1/2 -mr-[50vw] -ml-[50vw] w-screen border-t" /> */}
-      </main>
-    </>
+          ))}
+          <div className="border-green absolute bottom-8 w-full border-b"></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
@@ -117,6 +227,58 @@ async function StreakSquare() {
       <p className="text-3xl">
         {streak?.streak && streak.streak > 0 ? "🔥" : "❄️"}
       </p>
+    </div>
+  );
+}
+
+function LoadingPR() {
+  return <div className="bg-gray/50 h-26 w-26 animate-pulse rounded-lg" />;
+}
+
+function LoadingStreakSquare() {
+  return <div className="bg-gray/50 h-40 w-40 animate-pulse rounded-lg" />;
+}
+
+function LoadingTotalSquare() {
+  return <div className="bg-faded-green h-40 w-40 animate-pulse rounded-lg" />;
+}
+
+function LoadingBarChart() {
+  return (
+    <div className="mt-10 h-40 w-full animate-pulse rounded-lg">
+      <div className="relative flex h-full items-end justify-between p-2">
+        {Array.from({ length: 7 }).map((_, i) => (
+          <div
+            key={i}
+            className="flex h-full flex-col items-center justify-end"
+          >
+            <div className="mb-2">
+              <div
+                className="bg-gray w-8 rounded-t"
+                style={{
+                  height: `${Math.random() * 100}px`,
+                  minHeight: "0px",
+                }}
+                title={`${Math.random().toFixed(1)}%`}
+              />
+            </div>
+            <div className="border-gray absolute bottom-4 w-full border-b"></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function LoadingWorkoutCompletion() {
+  return (
+    <div className="mt-4 animate-pulse">
+      <div className="bg-foreground/10 relative h-3 w-full rounded-sm">
+        <div
+          className="bg-gray absolute inset-0 rounded-sm"
+          style={{ width: `${Math.floor(Math.random() * 100)}%` }}
+        ></div>
+      </div>
     </div>
   );
 }
