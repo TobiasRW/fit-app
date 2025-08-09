e41a5a0f6cf1b26edcac84062178958073cb3075
TobiasRW <tobiasrw98@gmail.com>
2025-08-09 18:41:04 +0200

    loading to session

--- src/app/(main)/session/[workoutSlug]/page.tsx
+++ src/app/(main)/session/[workoutSlug]/page.tsx
@@ -1,6 +1,8 @@
 import CurrentExerciseCard from "@/app/components/current-exercise-card";
 import { getCurrentWorkout } from "./actions";
 import CompleteWorkoutModal from "@/app/components/modals/complete-workout-modal";
+import ErrorCard from "@/app/components/error-card";
+import { Suspense } from "react";
 
 export default async function Page({
   params,
@@ -8,20 +10,30 @@ export default async function Page({
   params: Promise<{ workoutSlug: string }>;
 }) {
   const { workoutSlug } = await params;
+
+  return (
+    <main className="mx-auto mt-10 w-11/12 pb-20">
+      <Suspense fallback={<LoadingCurrentWorkout />}>
+        <CurrentWorkoutContent workoutSlug={workoutSlug} />
+      </Suspense>
+    </main>
+  );
+}
+
+async function CurrentWorkoutContent({ workoutSlug }: { workoutSlug: string }) {
   const workout = await getCurrentWorkout(workoutSlug);
 
   if ("error" in workout) {
     return (
-      <main className="mx-auto mt-10 w-11/12">
-        <h1 className="text-4xl font-bold">Error: {workout.error}</h1>
-      </main>
+      <div className="mt-8">
+        <ErrorCard errorText={workout.error} variant="primary" />
+      </div>
     );
   }
 
   return (
-    <main className="mx-auto mt-10 w-11/12 pb-20">
+    <>
       <h1 className="text-green text-4xl font-bold">{workout.name}</h1>
-
       <section className="mt-10">
         <div className="flex items-center justify-between">
           <h2 className="mb-2 text-2xl font-semibold">Exercises</h2>
@@ -39,6 +51,31 @@ export default async function Page({
           />
         ))}
       </section>
-    </main>
+    </>
+  );
+}
+
+function LoadingCurrentWorkout() {
+  return (
+    <>
+      <h1 className="text-gray dark:text-dark-gray text-4xl font-bold">
+        Workout
+      </h1>
+      <section className="mt-10">
+        <div className="flex items-center justify-between">
+          <h2 className="mb-2 text-2xl font-semibold">Exercises</h2>
+          <div className="bg-gray dark:bg-dark-gray h-5 w-18 animate-pulse rounded-full" />
+        </div>
+        <hr className="border-foreground/20 relative right-1/2 left-1/2 -mr-[50vw] -ml-[50vw] w-screen border-t" />
+      </section>
+
+      <section className="mt-4">
+        <div className="animate-pulse">
+          <div className="bg-gray dark:bg-dark-gray mb-4 h-14 w-full rounded-lg" />
+          <div className="bg-gray dark:bg-dark-gray mb-4 h-14 w-full rounded-lg" />
+          <div className="bg-gray dark:bg-dark-gray mb-4 h-14 w-full rounded-lg" />
+        </div>
+      </section>
+    </>
   );
 }
