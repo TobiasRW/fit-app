d00cdcc2c3b980da649d450c2d505c2474ce3578
TobiasRW <tobiasrw98@gmail.com>
2026-01-01 17:06:48 +0100

    session loading state separated

--- src/app/(main)/session/[workoutSlug]/page.tsx
+++ src/app/(main)/session/[workoutSlug]/page.tsx
@@ -4,6 +4,7 @@ import CompleteWorkoutModal from "@/app/components/modals/complete-workout-modal
 import ErrorCard from "@/app/components/cards/error-card";
 import { Suspense } from "react";
 import SkipWorkoutModal from "@/app/components/modals/skip-workout-modal";
+import LoadingSession from "@/app/components/loaders/loading-session";
 
 export default async function Page({
   params,
@@ -14,7 +15,7 @@ export default async function Page({
 
   return (
     <main className="mx-auto mt-10 w-11/12 pb-20">
-      <Suspense fallback={<LoadingCurrentWorkout />}>
+      <Suspense fallback={<LoadingSession />}>
         <CurrentWorkoutContent workoutSlug={workoutSlug} />
       </Suspense>
     </main>
@@ -64,31 +65,6 @@ async function CurrentWorkoutContent({ workoutSlug }: { workoutSlug: string }) {
             workoutCompleted={workout.completed}
           />
         ))}
-      </section>
-    </>
-  );
-}
-
-function LoadingCurrentWorkout() {
-  return (
-    <>
-      <h1 className="text-gray dark:text-dark-gray text-4xl font-bold">
-        Workout
-      </h1>
-      <section className="mt-10">
-        <div className="flex items-center justify-between">
-          <h2 className="mb-2 text-2xl font-semibold">Exercises</h2>
-          <div className="bg-gray dark:bg-dark-gray h-5 w-18 animate-pulse rounded-full" />
-        </div>
-        <hr className="border-foreground/20 relative right-1/2 left-1/2 -mr-[50vw] -ml-[50vw] w-screen border-t" />
-      </section>
-
-      <section className="mt-4">
-        <div className="animate-pulse">
-          <div className="bg-gray dark:bg-dark-gray mb-4 h-14 w-full rounded-lg" />
-          <div className="bg-gray dark:bg-dark-gray mb-4 h-14 w-full rounded-lg" />
-          <div className="bg-gray dark:bg-dark-gray mb-4 h-14 w-full rounded-lg" />
-        </div>
       </section>
     </>
   );
